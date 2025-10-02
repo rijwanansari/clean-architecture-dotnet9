05e5550f50f5196b9915a4ae2347f31c9a1c1500
Satya Karki <spacegrlsatya@gmail.com>
2025-10-02 09:49:45 +0000

    Add enums for OrderStatus and PaymentMethod

---
+++ src/CleanArchitecture.Domain/Exceptions/OrderStatus.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CleanArchitecture.Domain.Exceptions;
+
+public enum OrderStatus
+{
+    Pending = 0,
+    Confirmed = 1,
+    Processing = 2,
+    Shipped = 3,
+    Delivered = 4,
+    Cancelled = 5
+}
+
---
+++ src/CleanArchitecture.Domain/Exceptions/PaymentMethod.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitecture.Domain.Exceptions;
+
+public enum PaymentMethod
+{
+    CreditCard = 0,
+    DebitCard = 1,
+    PayPal = 2,
+    BankTransfer = 3,
+    Cash = 4
+
+}
